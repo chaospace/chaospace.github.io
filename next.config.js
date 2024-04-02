22d826cd056fef92177deef486b4c8ba0a95cc76
chaospace <ichaospace@gmail.com>
2024-04-02 15:38:13 +0900

    [SETTING] 배포 시 jekyll설정 추가

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 const path = require("path");
 const isProduction = process.env.NODE_ENV === "production";
-const prefix = isProduction ? "https://chaospace.github.io/" : "";
+const prefix = ""; //isProduction ? "/." : ".";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "chaospace.github.io",
+        port: "",
+      },
+    ],
+  },
   experimental: {
     serverActions: false,
     appDir: true,
