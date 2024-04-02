985f34b3193bc96cb4477595c00a9a227c0eea84
chaospace <ichaospace@gmail.com>
2024-04-02 13:06:08 +0900

    [CONFIG] gh-pages 배포 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 const path = require("path");
-
+const isProduction = process.env.NODE_ENV === "production";
+const prefix = isProduction ? "https://chaospace.github.io/" : "";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
   experimental: {
-    serverActions: true,
+    serverActions: false,
     appDir: true,
   },
+  assetPrefix: prefix,
   sassOptions: {
     includePaths: [path.join(__dirname, "css")],
   },
